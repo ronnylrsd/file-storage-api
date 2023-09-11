c80efc81ee9f4245e1f6582c16ed44e08e1b9e9e
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-09-11 19:56:59 -0300

    Feat: create project

---
+++ src/main/java/br/com/ronnyribeiro/filestorageapi/FileStorageApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ronnyribeiro.filestorageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageApiApplication.class, args);
+	}
+
+}
