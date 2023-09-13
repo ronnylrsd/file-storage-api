1e357693781bed2a81b2af882d30389e0c708087
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-09-13 09:09:15 -0300

    Feat: add getter and setter to uploadDir

--- src/main/java/br/com/ronnyribeiro/filestorageapi/FileStorageProperties.java
+++ src/main/java/br/com/ronnyribeiro/filestorageapi/FileStorageProperties.java
@@ -7,4 +7,12 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "file")
 public class FileStorageProperties {
     private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
 }
