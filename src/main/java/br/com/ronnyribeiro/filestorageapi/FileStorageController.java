30508974c495b36e6c68d095af71375052afaa8c
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-09-15 11:56:55 -0300

    Feat: add download

--- src/main/java/br/com/ronnyribeiro/filestorageapi/FileStorageController.java
+++ src/main/java/br/com/ronnyribeiro/filestorageapi/FileStorageController.java
@@ -1,17 +1,26 @@
 package br.com.ronnyribeiro.filestorageapi;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 
@@ -41,6 +50,27 @@ public class FileStorageController {
             return ResponseEntity.ok("Upload completed! Download Link: "+ fileDownloadUri);
         } catch (IOException ex){
             return ResponseEntity.badRequest().body("Could not store file " + fileName + ". Please try again!");
+        }
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws IOException{
+        Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+
+        try {
+            Resource resource = new UrlResource(filePath.toUri());
+            String contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+
+            if(contentType == null){
+                contentType = "application/octet-stream";
+            }
+
+            return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+        } catch (MalformedURLException e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 }
